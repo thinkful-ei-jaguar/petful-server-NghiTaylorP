3eadda716426db35664ee944725cdb18cb3accc6
Taylor Piccarreto <tpic12@gmail.com>
2020-03-13 18:16:13 -0400

    fixish

--- petful-server-template/config.js
+++ petful-server-template/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
-  CLIENT_ORIGIN : 'http://localhost:3000/' || 'https://petful-taylorp-nghit.now.sh/'
+  CLIENT_ORIGIN : process.env.CLIENT_ORIGIN || 'https://petful-taylorp-nghit.now.sh/'
 }
