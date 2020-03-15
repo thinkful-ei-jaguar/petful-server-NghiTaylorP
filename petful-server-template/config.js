14c70343065352f4668045cfc09d5d50583b6062
Taylor Piccarreto <tpic12@gmail.com>
2020-03-15 19:20:46 -0400

    Client origin spelled wrong...

--- petful-server-template/config.js
+++ petful-server-template/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
-  CLIENT_ORIGIN : 'https://petful-taylorp-nghit.now.sh/' || process.env.CLIENT_ORIGIN
+  CLIENT_ORIGIN : 'https://petful-taylorp-nghit.now.sh' || process.env.CLIENT_ORIGIN
 }
