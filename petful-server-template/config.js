91efd4a269543a9c1614b99a66437d9c0e7d5e90
Taylor Piccarreto <tpic12@gmail.com>
2020-03-15 12:46:49 -0400

    cors issue

--- petful-server-template/config.js
+++ petful-server-template/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
-  CLIENT_ORIGIN : process.env.CLIENT_ORIGIN || 'https://petful-taylorp-nghit.now.sh/'
+  CLIENT_ORIGIN : 'https://petful-taylorp-nghit.now.sh/'
 }
