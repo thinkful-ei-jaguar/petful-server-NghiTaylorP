48493dfe72064695de7e060cc4bcfe3707a9da82
Taylor Piccarreto <tpic12@gmail.com>
2020-03-21 17:04:17 -0400

    added random pet enqueue feature

--- petful-server-template/config.js
+++ petful-server-template/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
-  CLIENT_ORIGIN : process.env.CLIENT_ORIGIN
+  CLIENT_ORIGIN :'https://petful-taylorp-nghit.now.sh' || process.env.CLIENT_ORIGIN
 }
 //'https://petful-taylorp-nghit.now.sh' ||
